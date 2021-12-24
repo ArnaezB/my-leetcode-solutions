26ae0c73a701161b3157d9b6c20ea1aff9333af7
Borja <borja.arnaez@alumnos.uva.es>
2021-12-24 21:29:00 +0100

    problem56-improved

--- medium/problem56.java
+++ medium/problem56.java
@@ -15,40 +15,22 @@ public class problem56 {
     }
 
     public static int[][] merge(int[][] intervals) {
-        ArrayList<ArrayList<Integer>> merge = new ArrayList<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<int[]> merge = new ArrayList<>();
         
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]); // Sort input
 
-        for (int i: intervals[0]){ // Consider first element merged
-            list.add(i);
-        }
-        merge.add(list);
+        merge.add(intervals[0]);
 
         for (int[] i: intervals) {
-            if (i[0] < merge.get(merge.size()-1).get(1)) { 
+            if (i[0] <= merge.get(merge.size()-1)[1]) { 
                 // The next interval can be merged if its start is before or at the end of the previous
-                merge.get(merge.size()-1).set(0, Integer.min(merge.get(merge.size()-1).get(0), i[0]));
-                merge.get(merge.size()-1).set(1, Integer.max(merge.get(merge.size()-1).get(1), i[1]));
+                merge.get(merge.size()-1)[0] = Integer.min(merge.get(merge.size()-1)[0], i[0]);
+                merge.get(merge.size()-1)[1] = Integer.max(merge.get(merge.size()-1)[1], i[1]);
             } else { // Cannot be merged
-                list = new ArrayList<>();
-                for (int ii: i){
-                    list.add(ii);
-                }
-                merge.add(list);
+                merge.add(i);
             }
         }
         // Transform 2d-ArrayList to 2d-array
-        int[][] solution = new int[merge.size()][2];
-        int x = 0;
-        int y = 0;
-        for (ArrayList<Integer> l: merge) {
-            for (Integer i: l) {
-                solution[x][y++] = (int) i;
-            }
-            y=0;
-            x++;
-        }
-        return solution;
+        return merge.toArray(new int[merge.size()][]);
     }
 }
